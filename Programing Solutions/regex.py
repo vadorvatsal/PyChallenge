5bc13b9427de7c0d75814236df49c1e29ac0729c
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-09-19 11:49:12 -0500

    learning regular expression

---
+++ Programing Solutions/regex.py
@@ -0,0 +1,9 @@
+# This document contains how to work with regular expressions and few of examples to try
+# You need to use some regular expression engine to work with
+# Here we are using python engine and read the documentation if you need to know how to start with it
+
+# There are different flags which indicates modes of our regular expression
+# Possible modes are,
+#
+# Standard : /re/
+# Global   : /re/g
