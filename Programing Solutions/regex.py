46788145b104fe8a0fd2b8dd21fe9ae5cb05c4cc
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-09-19 11:49:57 -0500

    regular expression commit

--- Programing Solutions/regex.py
+++ Programing Solutions/regex.py
@@ -5,5 +5,26 @@
 # There are different flags which indicates modes of our regular expression
 # Possible modes are,
 #
-# Standard : /re/
-# Global   : /re/g
+# Standard          :   /re/
+# Global            :   /re/g
+# Case insensitive  :   /re/i
+# Multi line        :   /re/m
+
+# Keep in mind that searches are case-sensitive by default
+# famous UNIX based regex program is called grep which stands for g/re/p - global regular expression print
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------- #
+#                                                   Program starts here
+# ------------------------------------------------------------------------------------------------------------------------- #
+
+import re
+
+txt = "This is demo text on which over the time we will be performing different regular expression examples and try to grab or grep the matching pattern. Keep in mind that this is single line without any new line character."
+result = re.search("regular expression",txt)
+
+if result:
+    print("It's a match !")
+else:
+    print("No match")
