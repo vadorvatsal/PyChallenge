f0c49ff1faaab2b0e52c2f7360aaa039661f59e8
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-10-16 21:40:53 -0500

    added instructions for conversion process

--- convertToCSV.py
+++ convertToCSV.py
@@ -8,13 +8,15 @@ import csv
 
 # Opening JSON file and loading the data
 # into the variable data
+print("\nOpening json file . . .\n")
 with open('data.json') as json_file:
     data = json.load(json_file)
 
-employee_data = data['emp_details']
+print("\nFetching data . . .\n")
+articles_data = data['articles']
 
 # now we will open a file for writing
-data_file = open('data_file.csv', 'w')
+data_file = open('articles.csv', 'w')
 
 # create the csv writer object
 csv_writer = csv.writer(data_file)
@@ -22,15 +24,16 @@ csv_writer = csv.writer(data_file)
 # Counter variable used for writing
 # headers to the CSV file
 count = 0
-
-for emp in employee_data:
+print("\nCreating CSV file\n")
+for article in articles_data:
     if count == 0:
         # Writing headers of CSV file
-        header = emp.keys()
+        header = article.keys()
         csv_writer.writerow(header)
         count += 1
 
     # Writing data of CSV file
-    csv_writer.writerow(emp.values())
+    csv_writer.writerow(article.values())
 
 data_file.close()
+print("\nCompiled Successfully . . .\n")
