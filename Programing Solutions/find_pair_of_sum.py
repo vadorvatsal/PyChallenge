e997ab63617830b4e24f7a1ed4821f06a8bf3b3e
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-11-02 22:01:04 -0600

    solved

--- Programing Solutions/find_pair_of_sum.py
+++ Programing Solutions/find_pair_of_sum.py
@@ -9,4 +9,16 @@ Found pair at index 1 and 4 (7+3)
 '''
 
 
+arr = [1,1,1,1,1,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,5,2,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
+sum = 10
+flag = False
+dict ={}
 
+for i,e in enumerate(arr):
+    if (sum - e) in dict:
+        print("Found pair at index {} and {} ".format(dict.get(sum-e),i))
+        flag = True
+    dict[e]=i
+
+if not flag:
+    print("No pair found")
