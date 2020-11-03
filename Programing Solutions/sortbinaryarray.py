45171625c63c82ee3583654ebad4fe72cceeacc2
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-11-03 16:15:57 -0600

    quick sort

---
+++ Programing Solutions/sortbinaryarray.py
@@ -0,0 +1,11 @@
+"""
+Given a binary array, sort it in linear time and constant space. Output should print contain all zeroes followed
+by all ones.
+
+Input: {1,0,1,0,1,0,0,1}
+Output: {0,0,0,0,1,1,1,1}
+"""
+
+question = [2, 34, 467, 2, 0, 1, 7, 8, 9, 6, 4, 33333, 5456, 675, 234, 878, 979, 134, 7, 85, 456, 345, 345, 234, 765,
+            876, 8889, 5]
+
