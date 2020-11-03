1f67708ed5feb289534880861789c41dc02c8295
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-11-02 19:06:50 -0600

    find sum challenge

---
+++ Programing Solutions/find_pair_of_sum.py
@@ -0,0 +1,12 @@
+'''
+
+Given an unsorted array of integers, find a pair with a given sum in it
+
+Example, arr=[8,7,2,5,3,1] & sum=10
+Found pair at index 0 and 2 (8+2)
+Found pair at index 1 and 4 (7+3)
+
+
+'''
+
+
