c5538cb601adbb2a594053625482bbcf5f4d382b
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-11-07 15:30:56 -0600

    added simple test

--- selenium_test/select_test.py
+++ selenium_test/select_test.py
@@ -1,5 +1,19 @@
-from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('www.dev.to/hello_vatsal/')
 # % xattr -r -d com.apple.quarantine geckodriver to bypass the security warning in MacOS
 # https://firefox-source-docs.mozilla.org/testing/geckodriver/Notarization.html
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Firefox()
+browser.get('http://python.org')
+
+search = browser.find_element_by_name('q')
+search.clear()
+
+search.send_keys("pycon")
+search.send_keys(Keys.RETURN)
+time.sleep(5)
+
+browser.close()
+
