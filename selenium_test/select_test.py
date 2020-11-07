3c84345ed5020bb466cc2d3455e4c4805e19c55e
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2020-11-07 15:22:05 -0600

    comment added

--- selenium_test/select_test.py
+++ selenium_test/select_test.py
@@ -1,3 +1,5 @@
 from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('www.dev.to/hello_vatsal/')
+# % xattr -r -d com.apple.quarantine geckodriver to bypass the security warning in MacOS
+# https://firefox-source-docs.mozilla.org/testing/geckodriver/Notarization.html
