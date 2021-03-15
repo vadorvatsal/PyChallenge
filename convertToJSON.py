2a2c65b3cab0433e7b71ce3cbc5c91ee27a4eebf
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-03-15 01:22:40 -0600

    formatted json output

--- convertToJSON.py
+++ convertToJSON.py
@@ -2,9 +2,9 @@
 
 
 # Method 1
-import excel2json
-EXCEL_FILE = '/Users/Username/Desktop/Book1.xlsx'
-excel2json.convert_from_file(EXCEL_FILE)
+# import excel2json
+# EXCEL_FILE = '/Users/Username/Desktop/Book1.xlsx'
+# excel2json.convert_from_file(EXCEL_FILE)
 
 
 # Method 2
@@ -13,7 +13,9 @@ import json
 #
 excel_data_df = pandas.read_excel('/path/to/excel/file.xlsx', sheet_name='Sheet1')
 json_str = excel_data_df.to_json(orient='records')
+json_clean_str = json_str.replace("\'", '"')
+json_clean_str = json_clean_str.replace("\/", "/")
 
 
 with open('resultjson.json','w') as outfile:
-    json.dump(json_str, outfile, ensure_ascii=False, indent=4)
+    json.dump(json_clean_str, outfile, ensure_ascii=False, indent=4)
