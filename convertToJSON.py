ee26d17da060f30b3c3fef4dd07eb24070c0964f
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-03-15 01:10:19 -0600

    method 2 completed

--- convertToJSON.py
+++ convertToJSON.py
@@ -8,10 +8,12 @@ excel2json.convert_from_file(EXCEL_FILE)
 
 
 # Method 2
-# import pandas
-# import json
+import pandas
+import json
 #
-# excel_data_df = pandas.read_excel('/path/to/excel/file.xlsx', sheet_name='Sheet1')
-# json_str = excel_data_df.to_json(orient='records')
-# json_str = json.dumps(json_str, ensure_ascii=False, indent=4)
+excel_data_df = pandas.read_excel('/path/to/excel/file.xlsx', sheet_name='Sheet1')
+json_str = excel_data_df.to_json(orient='records')
 
+
+with open('resultjson.json','w') as outfile:
+    json.dump(json_str, outfile, ensure_ascii=False, indent=4)
