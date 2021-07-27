7e297936cf249a60c33340d0a940cc8cf2ed8cf7
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-07-27 03:10:23 -0600

    more descriptive with user feedback

--- Automation/quotedFile.py
+++ Automation/quotedFile.py
@@ -1,11 +1,20 @@
-import sys
+import sys,os,time
 import tkinter as tk
 from tkinter import filedialog
+try:
+    if sys.version_info[0]<3:
+        raise Exception
+except:
+    print("Not python version 3")
+    time.sleep(2)
 
 root = tk.Tk()
 root .withdraw()
 
+os.system('cls' if os.name == 'nt' else 'clear')
+print("Select source file")
 srcFilePath = filedialog.askopenfilename()
+print("Select destination directory")
 dstFileDir = filedialog.askdirectory()
 dstFilePath = "".join((dstFileDir,"/processedFile.txt"))
 
@@ -17,9 +26,13 @@ try:
             if len(item.rstrip()) != 0:
                 f.write("\"%s\",\n" % item.rstrip().lstrip())
 
-        print("Successfully Processed")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Successfully Processed {} in {}".format(srcFilePath,dstFilePath))
+        time.sleep(2)
 except:
-    print(f"Unexpected Error Occurred {sys.exc_info()}")
+    print(sys.exc_info())
 
 finally:
     print("Program Execution Completed")
+    time.sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
