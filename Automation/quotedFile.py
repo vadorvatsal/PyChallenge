922b70572b289bb18e36bb3f537cb08642b424de
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-07-27 16:39:01 -0600

    correction in exception

--- Automation/quotedFile.py
+++ Automation/quotedFile.py
@@ -1,5 +1,4 @@
-import sys,os,time
-import tkinter as tk
+import sys,os,time, tkinter as tk
 from tkinter import filedialog
 try:
     if sys.version_info[0]<3:
@@ -7,10 +6,8 @@ try:
 except:
     print("Warning you are not using python version 3")
     time.sleep(2)
-
 root = tk.Tk()
 root .withdraw()
-
 os.system('cls' if os.name == 'nt' else 'clear')
 print("Select source file\nAccepted file formats: txt,rtf")
 srcFilePath = filedialog.askopenfilename()
@@ -18,7 +15,6 @@ os.system('cls' if os.name == 'nt' else 'clear')
 fileFormat = srcFilePath.split("/")[-1].split(".")[-1]
 print("Selected file : {}".format(srcFilePath.split("/")[-1]))
 print("Selected file format : {}".format(fileFormat))
-
 try:
     if fileFormat == "txt" or fileFormat == "rtf":
         print("\nSelect destination directory")
@@ -29,25 +25,17 @@ try:
         dstFilePath = "".join((dstFileDir,"/processedFile.txt"))
         time.sleep(3)
         List = open(srcFilePath).readlines()
-
         with open(dstFilePath,"w") as f:
             for item in List:
                 if len(item.rstrip()) != 0:
                     f.write("\"%s\",\n" % item.rstrip().lstrip())
-
             os.system('cls' if os.name == 'nt' else 'clear')
             print("Successfully Processed at : {}".format(dstFilePath))
             time.sleep(2)
+        os.system("notepad.exe {}".format(dstFilePath) if os.name == 'nt' else "open -e {}".format(dstFilePath))
 except:
     print("Unexpected error {} occurred.".format(sys.exc_info()))
-
-try:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    os.system("notepad.exe {}".format(dstFilePath) if os.name == 'nt' else "open -e {}".format(dstFilePath))
-
-except:
-    print("Unexpected error {} occurred.".format(sys.exc_info()))
-
+    time.sleep(3)
 finally:
     print("Program Execution Completed")
     time.sleep(2)
