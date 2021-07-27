3f74be194c9c22b7f18d6bd110261fc714a55909
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-07-26 20:01:05 -0600

    quoted file complete

---
+++ Automation/quotedFile.py
@@ -0,0 +1,24 @@
+import sys
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root .withdraw()
+
+srcFilePath = filedialog.askopenfilename()
+dstFilePath = filedialog.askopenfilename()
+
+try:
+    List = open(srcFilePath).readlines()
+
+    with open(dstFilePath,"w") as f:
+        for item in List:
+            if len(item.rstrip()) != 0:
+                f.write("\"%s\",\n" % item.rstrip().lstrip())
+
+        print("Successfully Processed")
+except:
+    print(f"Unexpected Error Occurred {sys.exc_info()}")
+
+finally:
+    print("Program Execution Completed")
