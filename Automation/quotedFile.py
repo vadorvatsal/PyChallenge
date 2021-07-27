3de4a396e76b70ee5cf7718e3c00094b0f7f4f57
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-07-27 16:08:20 -0600

    accept only txt and rtf

--- Automation/quotedFile.py
+++ Automation/quotedFile.py
@@ -12,29 +12,32 @@ root = tk.Tk()
 root .withdraw()
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print("Select source file")
+print("Select source file\nAccepted file formats: txt,rtf")
 srcFilePath = filedialog.askopenfilename()
 os.system('cls' if os.name == 'nt' else 'clear')
+fileFormat = srcFilePath.split("/")[-1].split(".")[-1]
 print("Selected file : {}".format(srcFilePath.split("/")[-1]))
-print("Select destination directory")
-dstFileDir = filedialog.askdirectory()
-os.system('cls' if os.name == 'nt' else 'clear')
-print("Selected file : {}".format(srcFilePath.split("/")[-1]))
-print("Selected directory : {}".format(dstFileDir.split("/")[-1]))
-dstFilePath = "".join((dstFileDir,"/processedFile.txt"))
-time.sleep(3)
+print("Selected file format : {}".format(fileFormat))
 
 try:
-    List = open(srcFilePath).readlines()
-
-    with open(dstFilePath,"w") as f:
-        for item in List:
-            if len(item.rstrip()) != 0:
-                f.write("\"%s\",\n" % item.rstrip().lstrip())
-
+    if fileFormat == "txt" or fileFormat == "rtf":
+        print("\nSelect destination directory")
+        dstFileDir = filedialog.askdirectory()
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("Successfully Processed at : {}".format(dstFilePath))
-        time.sleep(2)
+        print("Selected file : {}".format(srcFilePath.split("/")[-1]))
+        print("Selected directory : {}".format(dstFileDir.split("/")[-1]))
+        dstFilePath = "".join((dstFileDir,"/processedFile.txt"))
+        time.sleep(3)
+        List = open(srcFilePath).readlines()
+
+        with open(dstFilePath,"w") as f:
+            for item in List:
+                if len(item.rstrip()) != 0:
+                    f.write("\"%s\",\n" % item.rstrip().lstrip())
+
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Successfully Processed at : {}".format(dstFilePath))
+            time.sleep(2)
 except:
     print("Unexpected error {} occurred.".format(sys.exc_info()))
 
